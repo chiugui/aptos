2b7b9ec69152c9b636b0bf34b8ab5ae09498ae8c
chiugui <59639733+chiugui@users.noreply.github.com>
2022-04-08 11:57:20 +0800

    Create update_aptos.sh

---
+++ update_aptos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#1.关闭节点
+docker-compose down
+
+#2.删除docker volume
+docker volume rm aptos_node_db
+
+#3.删除文件重新下载
+# 下载 创世节点文件
+wget -O ./genesis.blob https://devnet.aptoslabs.com/genesis.blob
+# 下载 waypoint（可验证检查点）文件
+wget -O ./waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
+
+#4.更新节点镜像
+docker-compose pull
+
+#5.启动全节点
+docker-compose up -d
+
