05fc60648e51417ecdb020d3556d1e922cf3acda
chiugui <59639733+chiugui@users.noreply.github.com>
2022-04-11 10:52:18 +0800

    Update install_aptos.sh

--- install_aptos.sh
+++ install_aptos.sh
@@ -2,11 +2,11 @@
 
 #安装docker和docker-compose略
 SH_PWD=`pwd`
-DIR=/root
+DIR=/aptos
 i=1
-
 # 创建 aptos-node 目录并进入该目录，之后的操作都会在该目录下进行
-mkdir -p ${DIR}/aptos$i && cd ${DIR}/aptos$i
+rm -f ${SH_PWD}/perr_id.list
+mkdir -p ${DIR}/aptos$i/data  && cd ${DIR}/aptos$i
 # 下载 docker-compose 编排文件
 #wget -O ./docker-compose.yaml https://raw.githubusercontent.com/chiugui/aptos/main/docker-compose.yaml
 \cp ${SH_PWD}/docker-compose.yaml ./docker-compose.yaml
@@ -28,15 +28,22 @@ docker pull aptoslab/tools:devnet
 # 生成私钥和公钥
 docker run --rm aptoslab/tools:devnet sh -c "echo '开始生成私钥...' && aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file /root/private-key.txt && echo '\n\n开始生成公钥和 Peer ID...' && aptos-operational-tool extract-peer-from-file --encoding hex --key-file /root/private-key.txt --output-file /root/peer-info.yaml && echo '\n\n您的私钥：' && cat /root/private-key.txt && echo '\n\n您的公钥和 Peer ID 信息如下：' && cat /root/peer-info.yaml" >./key.txt 2>&1
 Private_key=`sed -n '/您的私钥/{n;p;}' ./key.txt`
+#Public_key和peer_id相同
 Public_key=`sed -n '/---/{n;p;}' ./key.txt |awk -F ':' '{print $1}'`
+
+# 修改密钥文件权限
+chmod 0400 ./key.txt
 
 # 修改配置文件
 sed -i "18 a \      identity:\n        type: \"from_config\"\n        key: \"${Private_key}\"\n        peer_id: \"${Public_key}\"" ./public_full_node.yaml
 
 # 启动docker
-docker network create aptos-network
 docker-compose up -d
-sleep 120
+echo "节点启动中，请稍后......"
+sleep 60
+echo "peer_id_${i}:${Public_key}" && echo "peer_id_${i}:${Public_key}" >> ${SH_PWD}/perr_id.list 2>&1
+curl -s 127.0.0.1:11${i}0/metrics | grep aptos_state_sync_version | grep type
 #查看节点信息
+echo "查看节点信息,请使用如下命令:"
 echo "curl 127.0.0.1:11${i}0/metrics 2> /dev/null | grep -m 1 peer_id"
 echo "curl 127.0.0.1:11${i}0/metrics 2> /dev/null | grep aptos_state_sync_version | grep type"
