333f70763d0ede0f6977aa1b1a1e0e428c25bf4d
osker <59639733+chiugui@users.noreply.github.com>
2022-05-06 15:13:39 +0800

    Update update_aptos_all.sh

--- update_aptos_all.sh
+++ update_aptos_all.sh
@@ -8,8 +8,7 @@ for i in `cat ${SH_DIR}/aptos_dir`
 do
         cd ${i}
         SN=`echo "${i}" | tr -cd "[0-9]"`
-        curl http://osker.ml/update_aptos.sh|sh -x
-        #sh -x update_aptos.sh
+        sh -x update_aptos.sh
         sleep 10s
         curl  127.0.0.1:11${SN}0/metrics | grep aptos_state_sync_version | grep type
         cd ../
